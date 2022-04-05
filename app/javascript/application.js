ef74b0f85e534366dd0b939f4f75c43ac361a6ea
David Nahum <nausito@gmail.com>
2022-04-04 18:48:01 -0600

    Add react

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Entry point for the build script in your package.json.
-import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails";
 
 // Stimulus controllers.
-import "./controllers"
+import "./controllers";
+import "./components/index.js";
